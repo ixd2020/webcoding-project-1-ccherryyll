29715b21462d8b1bace38152366de3e894b3c452
Glitch (quaint-speckled-smell) <none>
2020-12-09 02:52:31 +0000

    🚙⛷ Updated with Glitch

--- script.js
+++ script.js
@@ -3,3 +3,33 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
+
+/*Mobile reponsive*/
+function mobilemenu() {
+            var x = document.getElementById("mytopnav");
+
+            if (x.className === "topnav") {
+                x.className = "topnav responsive";
+            } else {
+                x.className = "topnav";
+            }
+        }
+  
+
+/*loading page*/
+        function loadingfinished() {
+            setTimeout(showpage, 2000);
+        }
+
+        function showpage() {
+            document.getElementById("loader").style.display = "none"
+        }
+
+$(function () {
+  'use strict';
+  $('i').click(function () {
+    $(this).next().animate({
+      width: '250px'
+    });
+  });
+});
